7002b6b2d47544bf239fa5493851713f7ba3e9c1
Tatiana Saenz <tatianasaenz@digitallaws.com.co>
2025-10-19 23:06:39 -0500

    refactor: update server API structure and dependencies

--- apps/server/src/api/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Request, Response } from 'express';
-// @ts-ignore: no type declarations for 'serverless-http' are installed
-import serverless from 'serverless-http';
-import app from '../index'; 
-
-const handler = serverless(app);
-export default async (req: Request, res: Response) => handler(req as any, res as any);
